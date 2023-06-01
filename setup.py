d79b957371f61ae98317ed9c729c8347f6225b66
valerianF <valerian.fraisse@gmail.com>
2023-06-01 13:10:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ URL = "https://github.com/valerianF/soundmonitor"
 EMAIL = "valerian.fraisse@mail.mcgill.ca"
 AUTHOR = "Valérian Fraisse"
 REQUIRES_PYTHON = ">=3.7"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 
 HERE = Path(__file__).parent
 
