706297e1ed95084ed0c3d9c1c1f3dbe818abf331
valerianF <valerian.fraisse@gmail.com>
2023-06-01 12:28:02 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+NAME = "soundmonitor"
+DESCRIPTION = "Long-term sound level measurements analysis in Python"
+URL = "https://github.com/valerianF/soundmonitor"
+EMAIL = "valerian.fraisse@mail.mcgill.ca"
+AUTHOR = "Valérian Fraisse"
+REQUIRES_PYTHON = ">=3.7"
+VERSION = "0.0.3"
+
+HERE = Path(__file__).parent
+
+try:
+    with open(HERE / "README.md", encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=["soundmonitor"],
+    install_requires=["matplotlib>=3.7.0", "numpy>=1.21.6", "pandas>=1.3.5"],
+    include_package_data=True,
+    license="Apache",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Acoustics",
+        "Topic :: Scientific/Engineering",
+    ],
+)
