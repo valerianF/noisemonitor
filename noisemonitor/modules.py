263f36af099e0f18e8cbe92b0aaf3adc0d50acfa
valerianF <valerian.fraisse@gmail.com>
2025-02-25 18:23:56 -0500

    Add nday function for computing number of days or week within decibel ranges

--- noisemonitor/modules.py
+++ noisemonitor/modules.py
@@ -136,7 +136,7 @@ class NoiseMonitor:
 
         Returns
         ----------
-        DataFrame: DataFrame with Leq and Lden values for each day or week.
+        DataFrame: DataFrame with Leq,24h and Lden values for each day or week.
         """
         self.validate_column(column)
 
@@ -155,7 +155,7 @@ class NoiseMonitor:
                 lden_values = compute_lden(group, column, values=values)
                 result = {
                     'Period': period,
-                    'Leq': leq_value,
+                    'Leq,24h': leq_value,
                     'Lden': lden_values['lden'][0]
                 }
                 if values:
@@ -168,95 +168,6 @@ class NoiseMonitor:
 
         result_df = pd.DataFrame(results).set_index('Period')
         return result_df
-    
-    def lden(
-        self, 
-        column: str, 
-        day1: Optional[str] = None, 
-        day2: Optional[str] = None, 
-        values: bool=True
-        ) -> pd.DataFrame:
-        """Return the Lden, a descriptor of noise level based on Leq over
-        a whole day with a penalty for evening (19h-23h) and night (23h-7h)
-        time noise. By default, an average Lden is computed that is 
-        representative of all the sound level data. Can return a Lden value
-        corresponding to specific days of the week.
-
-        Parameters
-        ----------
-        column: str
-            column name to use for calculations. Should contain LAeq values.
-        day1 (optional): str, a day of the week in english, case-insensitive
-            first day of the week included in the Lden computation.
-        day2 (optional): str, a day of the week in english, case-insensitive
-            last (included) day of the week in the Lden computation. If day2 
-            happens later in the week than day1 the average will be computed 
-            outside of these days.
-        values: bool, default False
-            If set to True, the function will return individual day, evening
-            and night values in addition to the lden.
-
-        Returns
-        ---------- 
-        dict: daily or weekly lden rounded to two decimals. Associated day,
-            evening and night values are returned if values is set to True.
-        """
-        self.validate_column(column)
-
-        temp = filter_by_days(self.df, day1, day2)
-
-        return compute_lden(temp, column, values=values)
-    
-    def leq(
-        self, 
-        column: str, 
-        hour1: int, 
-        hour2: int, 
-        day1: Optional[str] = None, 
-        day2: Optional[str] = None, 
-        stats: bool = True
-        ) -> pd.DataFrame:
-        """Return the equivalent level (and optionally statistical indicators)
-        between two hours of the day. Can return a value corresponding to 
-        specific days of the week.
-
-        Parameters
-        ----------
-        column: str
-            column name to use for calculations.
-        hour1: int, between 0 and 24
-            hour for the starting time of the daily average.
-        hour2: int, between 0 and 24
-            hour for the ending time of the daily average. If hour2 > hour1 
-            the average will be computed outside of these hours.
-        day1 (optional): str, a day of the week in english, case-insensitive
-            first day of the week included in the Lden computation.
-        day2 (optional): str, a day of the week in english, case-insensitive
-            last (included) day of the week in the Lden computation. If day2 
-            happens later in the week than day1 the average will be computed 
-            outside of these days.
-        stats: bool, default True
-            If set to True, the function will return L10, L50 and L90 
-            together with the Leq.
-
-        Returns
-        ---------- 
-        float: daily or weekly equivalent level rounded to two decimals. 
-        Statistical indicators are included if stats is set to True.
-        """
-        self.validate_column(column)
-
-        temp = filter_by_days(self.df, day1, day2)
-        array = filter_by_hours(temp, hour1, hour2)[column]
-
-        if stats:
-            return pd.DataFrame({
-                'leq': [np.round(equivalent_level(array), 2)],
-                'l10': [np.round(np.nanpercentile(array, 90), 2)],
-                'l50': [np.round(np.nanpercentile(array, 50), 2)],
-                'l90': [np.round(np.nanpercentile(array, 10), 2)]
-            })
-        return pd.DataFrame({'leq': [np.round(equivalent_level(array), 2)]})
     
     def daily_weekly_number_of_noise_events(
         self,
@@ -392,6 +303,198 @@ class NoiseMonitor:
             index=event_times, 
             data={'Average NNEs': average_event_counts}
         )
+    
+    def lden(
+        self, 
+        column: str, 
+        day1: Optional[str] = None, 
+        day2: Optional[str] = None, 
+        values: bool=True
+        ) -> pd.DataFrame:
+        """Return the Lden, a descriptor of noise level based on Leq over
+        a whole day with a penalty for evening (19h-23h) and night (23h-7h)
+        time noise. By default, an average Lden is computed that is 
+        representative of all the sound level data. Can return a Lden value
+        corresponding to specific days of the week.
+
+        Parameters
+        ----------
+        column: str
+            column name to use for calculations. Should contain LAeq values.
+        day1 (optional): str, a day of the week in english, case-insensitive
+            first day of the week included in the Lden computation.
+        day2 (optional): str, a day of the week in english, case-insensitive
+            last (included) day of the week in the Lden computation. If day2 
+            happens later in the week than day1 the average will be computed 
+            outside of these days.
+        values: bool, default False
+            If set to True, the function will return individual day, evening
+            and night values in addition to the lden.
+
+        Returns
+        ---------- 
+        dict: daily or weekly lden rounded to two decimals. Associated day,
+            evening and night values are returned if values is set to True.
+        """
+        self.validate_column(column)
+
+        temp = filter_by_days(self.df, day1, day2)
+
+        return compute_lden(temp, column, values=values)
+    
+    def leq(
+        self, 
+        column: str, 
+        hour1: int, 
+        hour2: int, 
+        day1: Optional[str] = None, 
+        day2: Optional[str] = None, 
+        stats: bool = True
+        ) -> pd.DataFrame:
+        """Return the equivalent level (and optionally statistical indicators)
+        between two hours of the day. Can return a value corresponding to 
+        specific days of the week.
+
+        Parameters
+        ----------
+        column: str
+            column name to use for calculations.
+        hour1: int, between 0 and 24
+            hour for the starting time of the daily average.
+        hour2: int, between 0 and 24
+            hour for the ending time of the daily average. If hour2 > hour1 
+            the average will be computed outside of these hours.
+        day1 (optional): str, a day of the week in english, case-insensitive
+            first day of the week included in the Lden computation.
+        day2 (optional): str, a day of the week in english, case-insensitive
+            last (included) day of the week in the Lden computation. If day2 
+            happens later in the week than day1 the average will be computed 
+            outside of these days.
+        stats: bool, default True
+            If set to True, the function will return L10, L50 and L90 
+            together with the Leq.
+
+        Returns
+        ---------- 
+        float: daily or weekly equivalent level rounded to two decimals. 
+        Statistical indicators are included if stats is set to True.
+        """
+        self.validate_column(column)
+
+        temp = filter_by_days(self.df, day1, day2)
+        array = filter_by_hours(temp, hour1, hour2)[column]
+
+        if stats:
+            return pd.DataFrame({
+                'leq': [np.round(equivalent_level(array), 2)],
+                'l10': [np.round(np.nanpercentile(array, 90), 2)],
+                'l50': [np.round(np.nanpercentile(array, 50), 2)],
+                'l90': [np.round(np.nanpercentile(array, 10), 2)]
+            })
+        return pd.DataFrame({'leq': [np.round(equivalent_level(array), 2)]})
+    
+    def nday(
+        self, 
+        column: str, 
+        indicator: str = 'Leq,24h', 
+        bins: Optional[List[int]] = None,
+        thresholds: List[int] = [55, 60, 65],
+        plot: bool = False,
+        title: str = None,
+        figsize: tuple = (10,8), 
+        freq: str = 'D'
+    ) -> Union[pd.DataFrame, None]:
+        """Compute the number of days in a dataset for which the indicators 
+        are between given values of decibels.
+
+        Parameters
+        ----------
+        column: str
+            Column name to use for calculations.
+        indicator: str, default 'Leq,24h'
+            Indicator to use for the computation. Options are 'Leq,24h', 'Lden', 
+            'Lday', 'Levening', and 'Lnight'.
+        bins: list of int, optional
+            List of decibel values to define the bins. By default: <40 dBA 
+            and every 5 dBA until >=80 dBA.
+        thresholds: list of int, default [55, 60, 65]
+            Thresholds for color coding. 
+        plot: bool, default False
+            If set to True, the function will plot a histogram with color 
+            coding instead of returning a dataframe
+        figsize: tuple, default (10,8)
+            figure size in inches.
+        freq: str, default 'D'
+            Frequency for the computation. 'D' for daily and 'W' for weekly.
+
+        Returns
+        ----------
+        DataFrame or None: If plot is set to False, DataFrame with the number
+        of days for each decibel range.
+        """
+        if bins is None:
+            bins = [40, 45, 50, 55, 60, 65, 70, 75, 80]
+
+        # Compute daily or weekly indicators
+        indicators_df = self.daily_weekly_indicators(column, freq=freq, values=True)
+
+        # Validate the indicator
+        if indicator not in indicators_df.columns:
+            raise ValueError(
+                f"Invalid indicator. Choose from {indicators_df.columns.tolist()}")
+
+        # Count the number of days for each decibel range
+        counts = pd.cut(
+            indicators_df[indicator], 
+            bins=[-float('inf')] + bins + [float('inf')], 
+            right=False
+            ).value_counts().sort_index()
+
+        # Create a DataFrame for the counts
+        counts_df = pd.DataFrame(counts).reset_index()
+        counts_df.columns = ['Decibel Range', 'Number of Days']
+
+        # Plot the histogram if requested
+        if plot:
+            plt.figure(figsize=figsize)
+            plt.rcParams.update({'font.size': 16})
+
+            colors = ['#89dc35' if x.left < thresholds[0] \
+                      else '#dcdc35' if x.left < thresholds[1] \
+                      else '#dc8935' if x.left < thresholds[2] \
+                      else '#dc3535' for x in counts.index]
+
+            
+            ax = counts.plot(
+                kind='bar', 
+                color=colors, 
+                width=0.8, 
+                zorder=3
+                )
+            ax.grid(True, linestyle='--', zorder=0)
+            plt.xlabel('Decibel Range (dBA)')
+
+            if freq == 'D':
+                plt.ylabel('Number of Days')
+            elif freq == 'W':
+                plt.ylabel('Number of Weeks')            
+
+            # Set custom y-tick labels
+            ytick_labels = [f'<{bins[0]}'] + \
+                [f'{bins[i]}-{bins[i+1]}' for i in range(len(bins)-1)] + \
+                [f'>{bins[-1]}']
+            ax.set_xticklabels(ytick_labels)
+
+            if title is not None:
+                plt.title(title)
+
+            plt.xticks(rotation=45)
+            plt.tight_layout()
+            plt.show()
+
+            return
+
+        return counts_df
     
     def sliding_average(
         self, 
